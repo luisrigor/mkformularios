48bdd068a243a57bfc06e9fdbdc687dea93f123f
luisrigor <luis.parra@rigorcg.pt>
2023-06-22 09:46:59 -0500

    add entity PVMCarModelYearForecast

--- src/main/java/com/gsc/mkformularios/model/entity/PVMCarModelYearForecast.java
+++ src/main/java/com/gsc/mkformularios/model/entity/PVMCarModelYearForecast.java
@@ -10,10 +10,20 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "")
+@Table(name = "PVM_CARMODEL_YEAR_FORECASTS")
 public class PVMCarModelYearForecast {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "ID_CARMODEL")
+    private Integer idCarModel;
+    @Column(name = "YEAR")
+    private Integer year;
+    @Column(name = "MONTH")
+    private Integer month;
+    @Column(name = "FORECAST")
+    private Integer forecast;
+
 }
