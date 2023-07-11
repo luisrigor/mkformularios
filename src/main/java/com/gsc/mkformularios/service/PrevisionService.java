afdf4c07dd35f9a78f7bbe5b512991a088e9d77b
luisrigor <luis.parra@rigorcg.pt>
2023-07-11 12:44:40 -0500

    delete .class

--- src/main/java/com/gsc/mkformularios/service/PrevisionService.java
+++
@@ -1,9 +0,0 @@
-package com.gsc.mkformularios.service;
-
-import com.gsc.mkformularios.dto.PrevisionDTO;
-import com.gsc.mkformularios.security.UserPrincipal;
-
-public interface PrevisionService{
-
-    PrevisionDTO getUsedCarsAllPrevisionSalesMonth(UserPrincipal userPrincipal);
-}
